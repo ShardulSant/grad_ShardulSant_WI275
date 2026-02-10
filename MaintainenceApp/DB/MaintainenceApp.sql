79136aa7888613d6ebe0dd532c16cb587fa7d787
Shardul <140959449+ShardulSant@users.noreply.github.com>
2026-02-10 11:42:42 +0530

    Update MaintainenceApp.sql

--- MaintainenceApp/DB/MaintainenceApp.sql
+++ MaintainenceApp/DB/MaintainenceApp.sql
@@ -3,6 +3,9 @@ Drop table if exists OwnerDetails;
 Create table SiteDetails(Type Varchar(20),Size_X Int,Size_Y Int,Maintainence Int,Paid Int,SiteId Int GENERATED ALWAYS AS IDENTITY (START WITH 100),  Primary Key(SiteId));
 select * from SiteDetails;
 Create table OwnerDetails(Name Varchar(20),PaidAmount Int,id Int GENERATED ALWAYS AS IDENTITY (START WITH 1000),SiteId INT,Constraint siteid_foreign_key FOREIGN KEY (SiteId) REFERENCES SiteDetails(SiteId),Primary Key(id));
+ALTER TABLE OwnerDetails
+ADD Status VARCHAR(10) DEFAULT 'PENDING';
+
 Select * from OwnerDetails;
 Insert into SiteDetails(Type, Size_X, Size_Y, Maintainence, Paid)
 Values
@@ -61,36 +64,35 @@ Insert into OwnerDetails(Name ,PaidAmount, siteId) Values
 ('a',100,100);
 select * from OwnerDetails;
 
-Insert into OwnerDetails(Name ,PaidAmount, siteId) Values
-('b',100,101),
-('c',100,102),
-('d',100,103),
-('e',100,104),
-('f',100,105),
-('g',100,106),
-('h',100,107),
-('i',100,108),
-('j',100,109),
-('k',100,110),
-('m',100,112),
-('n',100,113),
-('o',100,114),
-('p',100,115),
-('q',100,116),
-('r',100,117),
-('s',100,118),
-('t',100,119),
-('u',100,120),
-('v',100,121),
-('w',100,122),
-('x',100,123),
-('y',100,124),
-('z',100,125),
-('abc',100,126),
-('bcd',100,127),
-('cde',100,128),
-('def',100,129),
-('efg',100,130);
+Insert into OwnerDetails(Name ,PaidAmount, siteId,Status) Values
+('b',100,101,'APPROVED'),
+('c',100,102,'APPROVED'),
+('d',100,103,'APPROVED'),
+('e',100,104,'APPROVED'),
+('f',100,105,'APPROVED'),
+('g',100,106,'APPROVED'),
+('h',100,107,'APPROVED'),
+('i',100,108,'PENDING'),
+('j',100,109,'PENDING'),
+('k',100,110,'PENDING'),
+('m',100,112,'PENDING'),
+('n',100,113,'PENDING'),
+('o',100,114,'PENDING'),
+('p',100,115,'PENDING'),
+('q',100,116,'PENDING'),
+('r',100,117,'PENDING'),
+('s',100,118,'PENDING'),
+('t',100,119,'PENDING'),
+('u',100,120,'PENDING'),
+('v',100,121,'PENDING'),
+('w',100,122,'PENDING'),
+('x',100,123,'PENDING'),
+('y',100,124,'PENDING'),
+('z',100,125,'PENDING'),
+('abc',100,126,'PENDING'),
+('bcd',100,127,'PENDING'),
+('cde',100,128,'PENDING'),
+('def',100,129,'PENDING'),
+('efg',100,130,'PENDING');
 select * from OwnerDetails;
-
 
