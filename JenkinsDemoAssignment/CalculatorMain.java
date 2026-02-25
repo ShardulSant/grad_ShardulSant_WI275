a3c1bc4f9cdaf0aa91e933ec017357f6c876666b
Shardul <140959449+ShardulSant@users.noreply.github.com>
2026-02-25 14:02:42 +0530

    Add files via upload

---
+++ JenkinsDemoAssignment/CalculatorMain.java
@@ -0,0 +1,13 @@
+
+
+class CalculatorMain{
+	public static void main(String args[]){
+		Calculator c1 = new Calculator();
+		System.out.println(c1.add(10,20));
+		System.out.println(c1.sub(10,20));
+		System.out.println(c1.mul(10,20));
+		System.out.println(c1.div(100,20));
+		
+		System.out.println("Hello World");
+	}
+}
